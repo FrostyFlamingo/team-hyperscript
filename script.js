fefa40f2e5ce687e4c4bc845cdd52d8c49525338
Ayan Khan <87468523+AYAANKHAN2003@users.noreply.github.com>
2021-12-18 17:22:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
-        if(this.scrollY > 1000){
+        if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
             $('.scroll-up-btn').removeClass("show");
